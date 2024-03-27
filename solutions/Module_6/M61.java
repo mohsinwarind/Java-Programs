1c1a42c1f9a68255ba17f47c666f0535742c0602
mohsinwarind <mohsinramzan999@gmail.com>
2024-03-27 14:07:12 +0500

    Module 6 Task 1

---
+++ solutions/Module_6/M61.java
@@ -0,0 +1,26 @@
+class swapDemo{
+    int x ,y;
+    swapDemo(){
+      this.x = 10;
+      this.y = 100;
+    }
+    void swap(int x , int y){
+        int temp = this.x; 
+        this.x = this.y ;
+        this.y = temp;
+    }
+
+}
+class Light{
+    public static void main(String[] args){
+    swapDemo swap1 = new swapDemo();
+    System.out.println("--Before Swapping--");
+    System.out.println("Value of X is : "+swap1.x);
+    System.out.println("Value of Y is : "+swap1.y);
+
+    System.out.println("--After Swapping--");
+    swap1.swap(swap1.x, swap1.y);
+    System.out.println("Value of X is : "+swap1.x);
+    System.out.println("Value of Y is : "+swap1.y);
+    }
+}
