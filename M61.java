e21b46497017e5ea23ebd1a2c95fb461abb68a85
mohsinwarind <mohsinramzan999@gmail.com>
2024-03-27 13:56:16 +0500

    Module 6 First Task

---
+++ M61.java
@@ -0,0 +1,36 @@
+/*class swapDemo{
+    // Task 1 for Module 6 
+    int x , y ;
+    swapDemo(int x, int y){
+    this.x  = x;
+    this.y =y;
+    }
+    void swapVar(){
+        int temp;
+        temp = x ;
+        x = y;
+        y = temp;
+        System.out.println("The Value of X after swapping is "+x);
+        System.out.println("The Value of Y after swapping is "+ y);
+    }
+} 
+
+
+public class M61 {
+
+    public static void main(String[] args ){
+        swapDemo swap = new swapDemo(10,100);
+
+        System.out.println("Your Values of X before swapping is "+swap.x);
+        System.out.println("Your Values of Y before swapping is "+swap.y);
+
+        swap.swapVar();
+
+    
+
+    }
+}*/
+
+
+
+
