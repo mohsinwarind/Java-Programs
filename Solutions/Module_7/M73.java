f5edf15c00709fc0a9c74a21da85966be24be4cc
mohsinwarind <mohsinramzan999@gmail.com>
2024-04-11 17:45:36 +0500

    Final Changing in Module 7

--- Solutions/Module_7/M73.java
+++ Solutions/Module_7/M73.java
@@ -9,11 +9,10 @@ class A{
         System.out.println("This is "+a);
     }
 }
-
 public class M73 {
     public static void main(String[] args){
         A a = new A(10);
     }
     
 }
-// Nope their isn't any error and the program compiles and runs well 
+// Nope their isn't any error and the program compile and run well 
